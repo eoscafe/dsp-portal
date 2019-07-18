fe21f0a180bf72241057d0d55e890f54e5de7c42
Syed Jafri <syed.jafri2@ucalgary.ca>
2019-07-18 05:59:57 -0600

    dist

---
+++ dist/precache-manifest.2bbdb8f21b456794c6c5f2e71a9385de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "22782a2e5b4d73e57418",
+    "url": "/js/chunk-vendors.409c314b.js"
+  },
+  {
+    "revision": "f9452c2decf6e144b5da",
+    "url": "/js/app.4c547092.js"
+  },
+  {
+    "revision": "26f1699cac59a0992d0a992b682d7d55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e79781b6a08bd5d36c32c22bb12aafba",
+    "url": "/img/partners/liquidapps.png"
+  },
+  {
+    "revision": "50adefba0e3304d959191bf0974af08e",
+    "url": "/css/semantic.min.css"
+  },
+  {
+    "revision": "22782a2e5b4d73e57418",
+    "url": "/css/chunk-vendors.f5ce28a8.css"
+  },
+  {
+    "revision": "112046db8cb25637248196beacc755d6",
+    "url": "/css/app.css"
+  },
+  {
+    "revision": "f9452c2decf6e144b5da",
+    "url": "/css/app.1df6f66a.css"
+  }
+];
